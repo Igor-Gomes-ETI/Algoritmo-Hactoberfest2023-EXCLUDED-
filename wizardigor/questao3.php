a088378dd0219f5ec92a0dbe0e33b90e0c564379
Igor Gomes <igorgomesads@gmail.com>
2023-10-06 14:51:39 -0300

    Update questao3.php
    criando um layout para a questão

--- wizardigor/questao3.php
+++ wizardigor/questao3.php
@@ -1,24 +1,71 @@
-<?php
-// Função para calcular a idade em dias
-function calcularIdadeEmDias($anos, $meses, $dias) {
-    // Define o número de dias em um ano, mês e dia
-    $dias_por_ano = 365;
-    $dias_por_mes = 30; // Usando uma média de 30 dias por mês
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de Idade em Dias</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding: 20px;
+        }
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #f9f9f9;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .input-group {
+            margin-bottom: 10px;
+        }
+        .input-group label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        .input-group input {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+        .result {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Calculadora de Idade em Dias</h1>
+        <div class="input-group">
+            <label for="anos">Anos:</label>
+            <input type="number" id="anos" placeholder="Digite a idade em anos">
+        </div>
+        <div class="input-group">
+            <label for="meses">Meses:</label>
+            <input type="number" id="meses" placeholder="Digite a idade em meses">
+        </div>
+        <div class="input-group">
+            <label for="dias">Dias:</label>
+            <input type="number" id="dias" placeholder="Digite a idade em dias">
+        </div>
+        <button onclick="calcularIdade()">Calcular Idade em Dias</button>
+        <div class="result" id="resultado"></div>
+    </div>
 
-    // Calcula a idade em dias
-    $idade_em_dias = ($anos * $dias_por_ano) + ($meses * $dias_por_mes) + $dias;
+    <script>
+        function calcularIdade() {
+            var anos = parseInt(document.getElementById('anos').value) || 0;
+            var meses = parseInt(document.getElementById('meses').value) || 0;
+            var dias = parseInt(document.getElementById('dias').value) || 0;
 
-    return $idade_em_dias;
-}
+            // Calcula a idade em dias
+            var idade_em_dias = (anos * 365) + (meses * 30) + dias;
 
-// Leitura da idade em anos, meses e dias
-$anos = intval(readline("Digite a idade em anos: "));
-$meses = intval(readline("Digite a idade em meses: "));
-$dias = intval(readline("Digite a idade em dias: "));
-
-// Chama a função para calcular a idade em dias
-$idade_em_dias = calcularIdadeEmDias($anos, $meses, $dias);
-
-// Exibe a idade em dias
-echo "A idade em dias é: " . $idade_em_dias . " dias\n";
-?>
+            // Exibe a idade em dias
+            document.getElementById('resultado').innerText = "A idade em dias é: " + idade_em_dias + " dias";
+        }
+    </script>
+</body>
+</html>
